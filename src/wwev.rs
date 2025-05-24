0554576998be95762d16aec6e58463d9c227468a
atampy25 <24306974+atampy25@users.noreply.github.com>
2025-05-24 17:52:52 +1000

    Fix WWEV model, including H1 support

--- src/wwev.rs
+++ src/wwev.rs
@@ -1,5 +1,6 @@
 use std::io::{Cursor, Read};
 
+use hitman_commons::game::GameVersion;
 use thiserror::Error;
 use tryvial::try_fn;
 
@@ -9,7 +10,6 @@ pub fn rune_module() -> Result<rune::Module, rune::ContextError> {
 
 	module.ty::<WwevError>()?;
 	module.ty::<WwiseEvent>()?;
-	module.ty::<WwiseEventData>()?;
 	module.ty::<WwiseNonStreamedAudioObject>()?;
 	module.ty::<WwiseStreamedAudioObject>()?;
 
@@ -30,7 +30,10 @@ pub enum WwevError {
 	InvalidNumber(#[from] std::num::TryFromIntError),
 
 	#[error("invalid utf-8: {0}")]
-	InvalidString(#[from] std::str::Utf8Error)
+	InvalidString(#[from] std::str::Utf8Error),
+
+	#[error("did not read the entire WWEV file")]
+	DidNotReadEntireFile
 }
 
 /// A Wwise event; a parsed WWEV file.
@@ -39,28 +42,18 @@ pub enum WwevError {
 #[cfg_attr(feature = "rune", derive(better_rune_derive::Any))]
 #[cfg_attr(feature = "rune", rune(item = ::hitman_formats::wwev))]
 #[cfg_attr(feature = "rune", rune_derive(DEBUG_FMT))]
+#[cfg_attr(feature = "rune", rune(constructor))]
 pub struct WwiseEvent {
 	/// The name of the event.
 	pub name: String,
 
 	pub event_max_attenuation: u32,
 
-	/// The event's data. Can be streamed or non-streamed.
-	pub data: WwiseEventData
-}
+	/// Non-streamed audio objects (all data is stored directly in the WWEV).
+	pub non_streamed: Vec<WwiseNonStreamedAudioObject>,
 
-#[derive(Clone, Debug, PartialEq)]
-#[cfg_attr(feature = "rune", derive(better_rune_derive::Any))]
-#[cfg_attr(feature = "rune", rune(item = ::hitman_formats::wwev))]
-#[cfg_attr(feature = "rune", rune_derive(DEBUG_FMT))]
-pub enum WwiseEventData {
-	/// One or more non-streamed audio objects (all data is stored directly in the WWEV).
-	#[cfg_attr(feature = "rune", rune(constructor))]
-	NonStreamed(#[cfg_attr(feature = "rune", rune(get, set))] Vec<WwiseNonStreamedAudioObject>),
-
-	/// One or more streamed audio objects (depending on WWEM files which contain the full data).
-	#[cfg_attr(feature = "rune", rune(constructor))]
-	Streamed(#[cfg_attr(feature = "rune", rune(get, set))] Vec<WwiseStreamedAudioObject>)
+	/// Streamed audio objects (depending on WWEM files which contain the full data).
+	pub streamed: Vec<WwiseStreamedAudioObject>
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -68,6 +61,7 @@ pub enum WwiseEventData {
 #[cfg_attr(feature = "rune", derive(better_rune_derive::Any))]
 #[cfg_attr(feature = "rune", rune(item = ::hitman_formats::wwev))]
 #[cfg_attr(feature = "rune", rune_derive(DEBUG_FMT))]
+#[cfg_attr(feature = "rune", rune(constructor))]
 pub struct WwiseNonStreamedAudioObject {
 	pub wem_id: u32,
 	pub data: Vec<u8>
@@ -78,6 +72,7 @@ pub struct WwiseNonStreamedAudioObject {
 #[cfg_attr(feature = "rune", derive(better_rune_derive::Any))]
 #[cfg_attr(feature = "rune", rune(item = ::hitman_formats::wwev))]
 #[cfg_attr(feature = "rune", rune_derive(DEBUG_FMT))]
+#[cfg_attr(feature = "rune", rune(constructor))]
 pub struct WwiseStreamedAudioObject {
 	/// The index of the WWEM dependency which contains the data for this object.
 	pub dependency_index: u32,
@@ -106,116 +101,110 @@ impl WwiseEvent {
 
 		let wwev_name = std::str::from_utf8(&wwev_name_data[0..wwev_name_data.len() - 1])?.to_owned();
 
-		let event_max_attenuation = u32::from_le_bytes({
+		// Max attenuation (seems to be big-endian for some reason)
+		let event_max_attenuation = u32::from_be_bytes({
 			let mut x = [0u8; 4];
 			wwev.read_exact(&mut x)?;
 			x
 		});
 
-		let non_streamed_count = i32::from_le_bytes({
+		let mut non_streamed_count = i32::from_le_bytes({
 			let mut x = [0u8; 4];
 			wwev.read_exact(&mut x)?;
 			x
 		});
 
-		// TODO: There is apparently another i32 here in 2016 WWEVs
+		// In H1, there's an unknown value that seems to always be 0xFFFFFFFF before the non-streamed count
+		if non_streamed_count == -1 {
+			// Advance to the actual non-streamed count
+			non_streamed_count = i32::from_le_bytes({
+				let mut x = [0u8; 4];
+				wwev.read_exact(&mut x)?;
+				x
+			});
+		}
 
-		if non_streamed_count == 0 {
-			// This is a streamed WWEV or it is simply empty
+		let mut non_streamed = vec![];
 
-			let entries_count = u32::from_le_bytes({
+		for _ in 0..non_streamed_count {
+			let wem_id = u32::from_le_bytes({
 				let mut x = [0u8; 4];
 				wwev.read_exact(&mut x)?;
 				x
 			});
 
-			let mut audio_objects = vec![];
+			let wem_size = u32::from_le_bytes({
+				let mut x = [0u8; 4];
+				wwev.read_exact(&mut x)?;
+				x
+			});
 
-			let mut cur_entry = 0;
+			let mut wem_data = vec![0; wem_size as usize];
+			wwev.read_exact(&mut wem_data)?;
 
-			while cur_entry < entries_count {
-				let wem_index = u32::from_le_bytes({
-					let mut x = [0u8; 4];
-					wwev.read_exact(&mut x)?;
-					x
+			non_streamed.push(WwiseNonStreamedAudioObject { wem_id, data: wem_data });
+		}
+
+		let streamed_count = u32::from_le_bytes({
+			let mut x = [0u8; 4];
+			wwev.read_exact(&mut x)?;
+			x
+		});
+
+		let mut streamed = vec![];
+
+		for _ in 0..streamed_count {
+			let wem_index = u32::from_le_bytes({
+				let mut x = [0u8; 4];
+				wwev.read_exact(&mut x)?;
+				x
+			});
+
+			let wem_id = u32::from_le_bytes({
+				let mut x = [0u8; 4];
+				wwev.read_exact(&mut x)?;
+				x
+			});
+
+			let prefetch_size = u32::from_le_bytes({
+				let mut x = [0u8; 4];
+				wwev.read_exact(&mut x)?;
+				x
+			});
+
+			if prefetch_size != 0 {
+				let mut prefetched_data = vec![0; prefetch_size as usize];
+				wwev.read_exact(&mut prefetched_data)?;
+
+				streamed.push(WwiseStreamedAudioObject {
+					dependency_index: wem_index,
+					wem_id,
+					prefetched_data: Some(prefetched_data)
 				});
-
-				let wem_id = u32::from_le_bytes({
-					let mut x = [0u8; 4];
-					wwev.read_exact(&mut x)?;
-					x
+			} else {
+				streamed.push(WwiseStreamedAudioObject {
+					dependency_index: wem_index,
+					wem_id,
+					prefetched_data: None
 				});
-
-				let prefetch_size = u32::from_le_bytes({
-					let mut x = [0u8; 4];
-					wwev.read_exact(&mut x)?;
-					x
-				});
-
-				if prefetch_size != 0 {
-					let mut prefetched_data = vec![0; prefetch_size as usize];
-					wwev.read_exact(&mut prefetched_data)?;
-
-					audio_objects.push(WwiseStreamedAudioObject {
-						dependency_index: wem_index,
-						wem_id,
-						prefetched_data: Some(prefetched_data)
-					});
-				} else {
-					audio_objects.push(WwiseStreamedAudioObject {
-						dependency_index: wem_index,
-						wem_id,
-						prefetched_data: None
-					});
-				}
-
-				cur_entry += 1;
 			}
+		}
 
-			WwiseEvent {
-				name: wwev_name,
-				event_max_attenuation,
-				data: WwiseEventData::Streamed(audio_objects)
-			}
-		} else {
-			// This WWEV has only non-streamed audio objects
+		if wwev.position() != wwev_data.len() as u64 {
+			return Err(WwevError::DidNotReadEntireFile);
+		}
 
-			let mut audio_objects = vec![];
-
-			let mut cur_entry = 0;
-
-			while cur_entry < non_streamed_count {
-				let wem_id = u32::from_le_bytes({
-					let mut x = [0u8; 4];
-					wwev.read_exact(&mut x)?;
-					x
-				});
-
-				let wem_size = u32::from_le_bytes({
-					let mut x = [0u8; 4];
-					wwev.read_exact(&mut x)?;
-					x
-				});
-
-				let mut wem_data = vec![0; wem_size as usize];
-				wwev.read_exact(&mut wem_data)?;
-
-				audio_objects.push(WwiseNonStreamedAudioObject { wem_id, data: wem_data });
-
-				cur_entry += 1;
-			}
-
-			WwiseEvent {
-				name: wwev_name,
-				event_max_attenuation,
-				data: WwiseEventData::NonStreamed(audio_objects)
-			}
+		WwiseEvent {
+			name: wwev_name,
+			event_max_attenuation,
+			non_streamed,
+			streamed
 		}
 	}
 
 	/// Serialise this WWEV.
 	#[cfg_attr(feature = "rune", rune::function(keep, instance))]
-	pub fn generate(self) -> Vec<u8> {
+	pub fn generate(self, version: GameVersion) -> Vec<u8> {
 		let mut wwev = vec![];
 
 		// Name
@@ -224,38 +213,34 @@ impl WwiseEvent {
 		wwev.push(0);
 
 		// Max attenuation
-		wwev.extend_from_slice(&self.event_max_attenuation.to_le_bytes());
+		wwev.extend_from_slice(&self.event_max_attenuation.to_be_bytes());
 
-		match self.data {
-			WwiseEventData::NonStreamed(ref audio_objects) => {
-				// Non-streamed count
-				wwev.extend_from_slice(&(audio_objects.len() as i32).to_le_bytes());
+		if version == GameVersion::H1 {
+			// Replicate the unknown value
+			wwev.extend_from_slice(&u32::MAX.to_le_bytes());
+		}
 
-				for audio_object in audio_objects {
-					wwev.extend_from_slice(&audio_object.wem_id.to_le_bytes());
-					wwev.extend_from_slice(&(audio_object.data.len() as u32).to_le_bytes());
-					wwev.extend_from_slice(&audio_object.data);
-				}
-			}
+		// Non-streamed count
+		wwev.extend_from_slice(&(self.non_streamed.len() as u32).to_le_bytes());
 
-			WwiseEventData::Streamed(ref audio_objects) => {
-				// Non-streamed count
+		for audio in self.non_streamed {
+			wwev.extend_from_slice(&audio.wem_id.to_le_bytes());
+			wwev.extend_from_slice(&(audio.data.len() as u32).to_le_bytes());
+			wwev.extend_from_slice(&audio.data);
+		}
+
+		// Streamed count
+		wwev.extend_from_slice(&(self.streamed.len() as u32).to_le_bytes());
+
+		for audio in self.streamed {
+			wwev.extend_from_slice(&audio.dependency_index.to_le_bytes());
+			wwev.extend_from_slice(&audio.wem_id.to_le_bytes());
+
+			if let Some(ref prefetched_data) = audio.prefetched_data {
+				wwev.extend_from_slice(&(prefetched_data.len() as u32).to_le_bytes());
+				wwev.extend_from_slice(prefetched_data);
+			} else {
 				wwev.extend_from_slice(&0u32.to_le_bytes());
-
-				// Entries count
-				wwev.extend_from_slice(&(audio_objects.len() as u32).to_le_bytes());
-
-				for audio_object in audio_objects {
-					wwev.extend_from_slice(&audio_object.dependency_index.to_le_bytes());
-					wwev.extend_from_slice(&audio_object.wem_id.to_le_bytes());
-
-					if let Some(ref prefetched_data) = audio_object.prefetched_data {
-						wwev.extend_from_slice(&(prefetched_data.len() as u32).to_le_bytes());
-						wwev.extend_from_slice(prefetched_data);
-					} else {
-						wwev.extend_from_slice(&0u32.to_le_bytes());
-					}
-				}
 			}
 		}
 
